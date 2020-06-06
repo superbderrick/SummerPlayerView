9e0771e4bf99975b4013de916d3ec0a103a1ba7d
Derrick <kang.derrick@gmail.com>
2020-06-06 20:30:47 +0900

    setup for basic configuration

---
+++ SummerPlayerView/SummerPlayerView/SummerPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  SummerPlayerView.swift
+//  SummerPlayerView
+//
+//  Created by derrick on 2020/06/06.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+
+open class SummerPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SummerPlayerViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SummerPlayerViewDemo
+//
+//  Created by derrick on 2020/06/06.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
