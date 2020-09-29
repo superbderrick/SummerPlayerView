29ab6601a9ffbad22269c578dadc22f73ad86d19
Derrick <kang.derrick@gmail.com>
2020-09-29 17:31:12 +0900

    improved ui

--- SummerPlayerView/SummerPlayerView/Player/Content.swift
+++ SummerPlayerView/SummerPlayerView/Player/Content.swift
@@ -6,10 +6,12 @@ public struct Content {
     let title: String
     let url: String
     let thumbnail: String
+    let totalTime: String
     
-    public init(title:String, url: String, thumbnail: String) {
+    public init(title:String, url: String, thumbnail: String , totalTime: String) {
         self.title = title
         self.url = url
         self.thumbnail = thumbnail
+        self.totalTime = totalTime
     }
 }
--- SummerPlayerViewDemo/PlayerViewController.swift
+++ SummerPlayerViewDemo/PlayerViewController.swift
@@ -23,11 +23,11 @@ class PlayerViewController: UIViewController, SummerPlayerViewDelegate  {
         playerView.sDelegate = self
     
         let playerItems = [
-            Content(title: "Paris", url: "https://content.jwplatform.com/manifests/yp34SRmf.m3u8", thumbnail: "paris"),
-            Content(title: "Busan", url: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8", thumbnail: "busan"),
-            Content(title: "Copenhagen", url: "https://content.jwplatform.com/manifests/yp34SRmf.m3u8", thumbnail: "cofenhagen"),
-            Content(title: "SF", url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8", thumbnail: "sf"),
-            Content(title: "Newyork", url: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8", thumbnail: "newyork"),
+            Content(title: "Paris", url: "https://content.jwplatform.com/manifests/yp34SRmf.m3u8", thumbnail: "paris" , totalTime: "11:32"),
+            Content(title: "Busan", url: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8", thumbnail: "busan",totalTime: "4:32"),
+            Content(title: "Copenhagen", url: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8", thumbnail: "cofenhagen",totalTime: "5:40"),
+            Content(title: "SF", url: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8", thumbnail: "sf",totalTime: "3:32"),
+            Content(title: "Newyork", url: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8", thumbnail: "newyork",totalTime: "4:32"),
         ]
         
         if let currentItem = playerItems.first {
