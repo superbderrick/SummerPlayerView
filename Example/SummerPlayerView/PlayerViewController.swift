f1360a1cd99c28be4cd670bcd983ecb51a97741d
Derrick <kang.derrick@gmail.com>
2020-10-01 23:54:56 +0900

    finish basic set up

---
+++ Example/SummerPlayerView/PlayerViewController.swift
@@ -0,0 +1,93 @@
+//
+//  PlayerViewController.swift
+//  SummerPlayerViewDemo
+//
+//  Created by derrick on 2020/08/18.
+//  Copyright © 2020 Derrick. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+
+class PlayerViewController: UIViewController  {
+    
+  //  let defaultConfig = DefaultConfig()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        let testContents = ContentsMaker.getContents()
+//        let sampleTheme = ThemeMaker.getTheme()
+//
+//        let summerPlayerView = SummerPlayerView(configuration: defaultConfig, theme: sampleTheme,targetView: view)
+//        summerPlayerView.delegate = self
+//
+//        if let currentItem = testContents.first {
+//            summerPlayerView.setPlayList(currentItem: currentItem, items: testContents)
+//        }
+//
+//        view.addSubview(summerPlayerView)
+//
+//        summerPlayerView.pinEdges(targetView: view)
+        
+    }
+    
+}
+
+//extension PlayerViewController : SummerPlayerViewDelegate {
+//    func didFinishVideo() {
+//
+//        if(self.defaultConfig.playbackMode == .quit) {
+//            moveViewController()
+//        }
+//
+//
+//    }
+//
+//    func didStartVideo() {
+//
+//    }
+//
+//    func didChangeSliderValue(_ seekTime: CMTime) {
+//
+//    }
+//    func didPressBackButton() {
+//        moveViewController()
+//    }
+//
+//    func didPressNextButton() {
+//
+//    }
+//
+//    func didPressPreviousButton() {
+//
+//    }
+//
+//    func didPressAirPlayButton() {
+//
+//    }
+//
+//    func didPressMoreButton() {
+//
+//    }
+//
+//    func didPressContentsListView(index: Int) {
+//
+//    }
+//
+//    func didPressPlayButton(isActive: Bool) {
+//
+//    }
+//
+//}
+
+extension PlayerViewController {
+    
+    fileprivate func moveViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "MainVC")
+        self.present(controller, animated: true, completion: nil)
+    }
+}
--- Example/SummerPlayerView/ViewController.swift
+++ Example/SummerPlayerView/ViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func startDemo(_ sender: Any) {
+        movePlayerController()
     }
+    
+}
 
+extension ViewController {
+    
+    fileprivate func movePlayerController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "PlayerVC")
+        
+        self.present(controller, animated: true, completion: nil)
+    }
+    
 }
 
